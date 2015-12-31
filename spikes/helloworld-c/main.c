9e0acc54e26b63d6e6e05e0970910f7c3fc90cd8
dschwald <dschwald@hotmail.fr>
2015-12-31 17:05:36 +0100

    ajout helloworld.c

---
+++ spikes/helloworld-c/main.c
@@ -0,0 +1,15 @@
+/* 
+pour compiler: 
+gcc main.c
+pour executer: 
+./a.out
+*/
+
+#include <stdio.h>
+
+int main() {
+        printf ("Hello, World!\n");
+        printf ("Hello, World!\n");
+        return 0;
+}
+
